143a5f0b096a7b961a0e51278b25b05c620690eb
MUTHURAJ1 <110025615+MUTHURAJ1@users.noreply.github.com>
2022-07-28 16:05:46 +0530

    Add files via upload

---
+++ Program/HelloWorld.java
@@ -0,0 +1,13 @@
+/*@author: Muthuraj T
+Date : 28/07/2022
+Title : HelloWorld Program
+Question : Executing Basic Java Program
+*/
+class HelloWorld
+{
+	// calling main method
+	public static void main (String[]args)
+	{
+		System.out.println("Hello I am JAVA");
+	}
+}
